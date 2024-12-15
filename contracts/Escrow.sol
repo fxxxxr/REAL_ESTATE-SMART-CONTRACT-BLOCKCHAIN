9d98301a702f7e545b6cea0384bd8385c3cac8f4
DEEPANSHU SINGH GAUTAM <ds5561674@gmail.com>
2024-12-15 18:09:25 +0530

    earnest contracts and listing

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -11,12 +11,25 @@ contract Escrow {
     address public inspector;
     address public lender;
 
+    modifier onlyBuyer(uint256 _nftID) {
+        require(msg.sender == buyer[_nftID], "Only buyer can call this method");
+        _;
+    }
+    modifier onlyseller() {
+        require(msg.sender == seller, "Only seller can call this method ");
+        _;
+    }
+    modifier onlyInspector(){
+        require(msg.sender == inspector,"only inspector can call this method");
+        _;
+    }
+
     //mapping
     mapping(uint256 => bool) public isListed;
     mapping(uint256 => uint256) public purchasePrice;
     mapping(uint256 => uint256) public escrowAmount;
     mapping(uint256 => address) public buyer;
-
+    mapping(uint256 => bool) public inspectionPassed;
     constructor(
         address _nftAddress,
         address payable _seller,
@@ -35,7 +48,7 @@ work to do : MAKE IT ONLYSELLER BY USING MODIFIER */
         address _buyer,
         uint256 _purchasePrice,
         uint _escrowAmount
-    ) public payable {
+    ) public payable onlyseller {
         // transfer of NFT from seller to this contract
         IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
         //mapping
@@ -44,5 +57,25 @@ work to do : MAKE IT ONLYSELLER BY USING MODIFIER */
         escrowAmount[_nftID] = _escrowAmount;
         buyer[_nftID] = _buyer;
     }
-}
 
+    //put under contract (only buyer - payable escrow)
+    function depositEarnest(uint256 _nftID) public payable onlyBuyer(_nftID) {
+        require(msg.value >= escrowAmount[_nftID]);
+    }
+
+
+    // updates the inspection status (only inspector)
+    function updateInspectionStatus(uint256 _nftID, bool _passed) 
+    public onlyInspector {
+       inspectionPassed[_nftID] = _passed;
+    }
+
+    //receive function
+    receive() external payable {}
+
+
+    // get balance
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
--- test/Escrow.js
+++ test/Escrow.js
@@ -92,4 +92,20 @@ describe('Listing', () =>{
         expect(result).to.be.equal(tokens(5))
     })   
 }) 
+describe('Deposits', () =>{
+    it('Updates contract balance', async () => {
+        const transaction = await escrow.connect(buyer).depositEarnest(1,{value : tokens(5)})
+        await transaction.wait()  
+        const result = await escrow.getBalance()
+        expect(result).to.be.equal(tokens(5))
+     })
+    })
+     describe('Inspection', () =>{
+        it('Updates inspection status', async () => {
+          const transaction = await escrow.connect(inspector).updateInspectionStatus(1, true) 
+          await transaction.wait()  
+          const result = await escrow.inspectionPassed(1)
+          expect(result).to.be.equal(true)
+        })
+    }) 
 })

99a00b5920778ab5e34cf9556a8650cb29731562
DEEPANSHU SINGH GAUTAM <ds5561674@gmail.com>
2024-12-16 20:08:49 +0530

    frontend Completed

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,8 @@ const Search = () => {
     return (
         <header>
             <h2 className="header__title">Search it. Explore it. Buy it.</h2>
+            
+            
             <input
                 type="text"
                 className="header__search"
